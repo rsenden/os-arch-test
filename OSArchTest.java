0139fa1a73251fb3959d95fc017aee80d5f2eede
Ruud Senden <8635138+rsenden@users.noreply.github.com>
2024-03-05 02:22:03 +0100

    test build failure

--- OSArchTest.java
+++ OSArchTest.java
@@ -1,5 +1,5 @@
 public class OSArchTest {
-    public static void main(String[] args) {
+    xpublic static void main(String[] args) {
         System.out.println(System.getProperty("os.name")+"/"+System.getProperty("os.arch"));
     }
 }
